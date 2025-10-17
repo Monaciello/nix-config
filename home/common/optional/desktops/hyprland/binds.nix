3163bcfc7dc2780ea52ee6868b276e4a69418c82
emergentmind <7410928+emergentmind@users.noreply.github.com>
2025-10-16 21:02:09 -0600

    refactor: keybind correction

--- home/common/optional/desktops/hyprland/binds.nix
+++ home/common/optional/desktops/hyprland/binds.nix
@@ -201,7 +201,7 @@
         #
         # ========== Misc ==========
         #
-        "SUPERSUPER, t, exec, arrangeTiles" # custom function in ./scripts.nix
+        "SHIFTSUPER, t, exec, arrangeTiles" # custom function in ./scripts.nix
         "SHIFTSUPER,r,exec,hyprctl reload" # reload the configuration file
         "SUPER,e,exec,wlogout" # display wlogout
       ];
