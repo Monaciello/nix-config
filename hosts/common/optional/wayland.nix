d716881c82896c7e7ed41e5e071d1711ba2a83cc
emergentmind <7410928+emergentmind@users.noreply.github.com>
2025-10-19 13:34:23 -0600

    refactor: disable swww for now until time to properly set up. log spam

--- hosts/common/optional/wayland.nix
+++ hosts/common/optional/wayland.nix
@@ -4,6 +4,5 @@
   environment.systemPackages = [
     pkgs.grim # screen capture component, required by flameshot
     pkgs.waypaper # wayland packages(nitrogen analog for wayland)
-    pkgs.swww # backend wallpaper daemon required by waypaper
   ];
 }
