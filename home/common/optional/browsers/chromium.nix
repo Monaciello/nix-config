1fe7ce581d6a4eb2d71e5e3746ffdcc62cf3f048
emergentmind <7410928+emergentmind@users.noreply.github.com>
2025-10-24 13:16:38 -0600

    fix: brave/chromium issue in hyprland until fix included in new release https://github.com/hyprwm/Hyprland/pull/11877

--- home/common/optional/browsers/chromium.nix
+++ home/common/optional/browsers/chromium.nix
@@ -4,6 +4,7 @@
     commandLineArgs = [
       "--no-default-browser-check"
       "--restore-last-session"
+      "--disable-features=WaylandWpColorManagerV1" # workaround for https://github.com/hyprwm/Hyprland/pull/11877 until the commit gets added to a new hyrpland release
     ];
   };
 }
