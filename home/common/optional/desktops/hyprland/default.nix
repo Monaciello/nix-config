3346aba34b1337c628e77e062c1f0d8f743b3783
emergentmind <7410928+emergentmind@users.noreply.github.com>
2025-10-25 10:37:42 -0600

    refactor: disable amdgpu_top autolaunch. not referenced enough

--- home/common/optional/desktops/hyprland/default.nix
+++ home/common/optional/desktops/hyprland/default.nix
@@ -182,7 +182,7 @@
             "[workspace 9 silent]brave"
             "[workspace 10 silent]virt-manager"
             "[workspace 10 silent]ghostty --title=btop -e btop"
-            "[workspace 10 silent]ghostty --title=amdgpu_top -e amdgpu_top --dark"
+            #"[workspace 10 silent]ghostty --title=amdgpu_top -e amdgpu_top --dark" # don't reference often enough
             "[workspace 10 silent]spotify"
             "[workspace special silent]/run/current-system/sw/bin/protonvpn-app"
             "[workspace special silent]yubioath-flutter"
