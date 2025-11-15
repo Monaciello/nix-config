dfa5e72341df87151dc3dcd27b63019177bf5650
emergentmind <7410928+emergentmind@users.noreply.github.com>
2025-11-14 21:09:03 -0700

    feat: add optional gnome, sddm, and x11 configs

---
+++ hosts/common/optional/gnome.nix
@@ -0,0 +1,43 @@
+{ pkgs, ... }:
+{
+  services.xserver.desktopManager.gnome.enable = true;
+  services.gnome.gnome-keyring.enable = true;
+
+  environment.gnome.excludePackages = builtins.attrValues {
+    inherit (pkgs)
+      gnome-photos
+      gnome-tour
+      gnome-text-editor # important otherwise overrides mime-types
+      gedit # text editor
+      gnome-console # favor gnome-terminal
+      cheese # webcam tool
+      atomix # puzzle game
+      epiphany # web browser
+      geary # email reader
+      gnome-characters
+      iagno # go game
+      hitori # sudoku game
+      tali # poker game
+      simple-scan
+      totem
+      # video player
+      gnome-maps
+      gnome-weather
+      gnome-music
+      yelp # Help view
+      gnome-contacts
+      gnome-initial-setup
+      ;
+
+  };
+  programs.dconf.enable = true;
+  environment.systemPackages = builtins.attrValues {
+  };
+  #services.touchegg.enable = true;
+
+  # Workaround for GNOME autologin: https://github.com/NixOS/nixpkgs/issues/103746#issuecomment-945091229
+  systemd.services."getty@tty1".enable = false;
+  systemd.services."autovt@tty1".enable = false;
+
+  systemd.oomd.enable = false; # Try to prrevent Gnome BSOD
+}
