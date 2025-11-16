a07ad6103752c90ccf8e0a3a9fb02031b1f5099d
emergentmind <7410928+emergentmind@users.noreply.github.com>
2025-11-16 13:33:38 -0700

    fix: declare unstable nix-direnv

--- home/common/core/direnv.nix
+++ home/common/core/direnv.nix
@@ -5,6 +5,9 @@
     package = pkgs.unstable.direnv;
     enableBashIntegration = true;
     enableZshIntegration = true;
-    nix-direnv.enable = true; # better than native direnv nix functionality - https://github.com/nix-community/nix-direnv
+    nix-direnv = {
+      enable = true; # better than native direnv nix functionality - https://github.com/nix-community/nix-direnv
+      package = pkgs.unstable.nix-direnv;
+    };
   };
 }
