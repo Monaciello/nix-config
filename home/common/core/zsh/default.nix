523d7da2c98f19dfe5a70859eb3fc1eb1af72ce7
emergentmind <7410928+emergentmind@users.noreply.github.com>
2025-11-22 14:26:20 -0700

    feat(starship): transient prompt for zsh and WIP colouring

--- home/common/core/zsh/default.nix
+++ home/common/core/zsh/default.nix
@@ -4,6 +4,41 @@
   lib,
   ...
 }:
+let
+  # transient prompt for zsh in starship
+  # there are other ways of doing this that allow customization of the
+  # transient prompt in starship config itself but we only use 'character' anyway
+  # see here for alternate solution ideas:https://github.com/starship/starship/discussions/5950
+  transientPrompt = ''
+    TRANSIENT_PROMPT=$(starship module character)
+
+    function zle-line-init() {
+        emulate -L zsh
+
+        [[ $CONTEXT == start ]] || return 0
+        while true; do
+            zle .recursive-edit
+            local -i ret=$?
+            [[ $ret == 0 && $KEYS == $'\4' ]] || break
+            [[ -o ignore_eof ]] || exit 0
+        done
+
+        local saved_prompt=$PROMPT
+        local saved_rprompt=$RPROMPT
+
+        PROMPT=$TRANSIENT_PROMPT
+        zle .reset-prompt
+        PROMPT=$saved_prompt
+
+        if (( ret )); then
+            zle .send-break
+        else
+            zle .accept-line
+        fi
+        return ret
+    }
+  '';
+in
 {
   #
   # ========= Programs integrated to zsh via option or alias =========
@@ -41,113 +76,57 @@
     # NOTE: zsh module will load *.plugin.zsh files by default if they are located in the src=<folder>, so
     # supply the full folder path to the plugin in src=. To find the correct path, atm you must check the
     # plugins derivation until PR XXXX (file issue) is fixed
+    plugins =
+      [
+        {
+          name = "zhooks";
+          src = "${pkgs.zhooks}/share/zsh/zhooks";
+        }
+        {
+          name = "you-should-use";
+          src = "${pkgs.zsh-you-should-use}/share/zsh/plugins/you-should-use";
+        }
+        # Allow zsh to be used in nix-shell
+        {
+          name = "zsh-nix-shell";
+          file = "nix-shell.plugin.zsh";
+          src = pkgs.fetchFromGitHub {
+            owner = "chisui";
+            repo = "zsh-nix-shell";
+            rev = "v0.8.0";
+            sha256 = "1lzrn0n4fxfcgg65v0qhnj7wnybybqzs4adz7xsrkgmcsr0ii8b7";
+          };
+        }
+      ]
 
-    plugins = [
-      # {
-      #   name = "powerlevel10k-config";
-      #   src = ./p10k;
-      #   file = "p10k.zsh.theme"; # NOTE: Don't use .zsh because of shfmt barfs on it, and can't ignore files
-      # }
-      # {
-      #   name = "zsh-powerlevel10k";
-      #   src = "${pkgs.zsh-powerlevel10k}/share/zsh-powerlevel10k/";
-      #   file = "powerlevel10k.zsh-theme";
-      # }
-      {
-        name = "zhooks";
-        src = "${pkgs.zhooks}/share/zsh/zhooks";
-      }
-      {
-        name = "you-should-use";
-        src = "${pkgs.zsh-you-should-use}/share/zsh/plugins/you-should-use";
-      }
-      # Allow zsh to be used in nix-shell
-      {
-        name = "zsh-nix-shell";
-        file = "nix-shell.plugin.zsh";
-        src = pkgs.fetchFromGitHub {
-          owner = "chisui";
-          repo = "zsh-nix-shell";
-          rev = "v0.8.0";
-          sha256 = "1lzrn0n4fxfcgg65v0qhnj7wnybybqzs4adz7xsrkgmcsr0ii8b7";
-        };
-      }
-    ]
+      # The iso doesn't use our overlays, so don't add custom packages
+      #FIXME:move these to an optional custom plugins module and remove iso check
+      ++ [
+        {
+          name = "zsh-term-title";
+          src = "${pkgs.zsh-term-title}/share/zsh/zsh-term-title/";
+        }
+        {
+          name = "cd-gitroot";
+          src = "${pkgs.cd-gitroot}/share/zsh/cd-gitroot";
+        }
+        {
+          name = "zsh-deep-autocd";
+          src = "${pkgs.zsh-deep-autocd}/share/zsh/zsh-deep-autocd";
+        }
+        {
+          name = "zsh-autols";
+          src = "${pkgs.zsh-autols}/share/zsh/zsh-autols";
+        }
+      ]
+      |> lib.optionals (config.hostSpec.hostName != "iso" && pkgs ? "zsh-term-title");
 
-    # The iso doesn't use our overlays, so don't add custom packages
-    #FIXME:move these to an optional custom plugins module and remove iso check
-    ++ lib.optionals (config.hostSpec.hostName != "iso" && pkgs ? "zsh-term-title") [
-      {
-        name = "zsh-term-title";
-        src = "${pkgs.zsh-term-title}/share/zsh/zsh-term-title/";
-      }
-      {
-        name = "cd-gitroot";
-        src = "${pkgs.cd-gitroot}/share/zsh/cd-gitroot";
-      }
-      {
-        name = "zsh-deep-autocd";
-        src = "${pkgs.zsh-deep-autocd}/share/zsh/zsh-deep-autocd";
-      }
-      {
-        name = "zsh-autols";
-        src = "${pkgs.zsh-autols}/share/zsh/zsh-autols";
-      }
-    ];
-
-    initExtra = ''
-      # The functions below rely on the 'starship init zsh' output to work.
-
-      # --- Transient Prompt Configuration Start ---
-
-      # Define a function to restore the full prompt (current Starship setup)
-      function starship_full_prompt {
-        # Prints the prompt as defined by Starship's full configuration
-        starship prompt --cmd-duration="$$(( EPOCHREALTIME - STARSHIP_START ))" --status="$?"
-      }
-
-      # Define a function for the "transient" prompt (e.g., just the status/time)
-      function starship_transient_prompt {
-        # Example transient prompt: just the status/time and an arrow
-        # You can customize this to be much simpler, e.g., '$? >'
-        starship prompt --status="$?" \
-                        --cmd-duration="$$(( EPOCHREALTIME - STARSHIP_START ))" \
-                        --layout 'promp_char$'
-      }
-
-      # Zsh precmd_functions: Executed just before the prompt is displayed.
-      # We use it to set the start time for starship's duration calculation.
-      zstyle ':starship:*:*:*' precmd_functions 'starship_precmd'
-
-      # Transient Prompt Configuration
-      # 1. Enable the Transient Prompt feature
-      # If this option is not set, zsh won't try to use 'precmd_x' functions for transient prompts.
-      setopt PROMPT_SP
-      # setopt PROMPT_SUBST  # Often set by Starship init, but good to check.
-
-      # 2. Use the defined functions for the prompts
-      # The full prompt function, executed *before* the command runs
-      # We want to display the *full* Starship prompt while the user types.
-      zle -N starship_full_prompt precmd
-
-      # The transient prompt function, executed *after* the command runs
-      # We want to display the *simple/transient* Starship prompt for history.
-      zle -N starship_transient_prompt precmd_x
-
-      # --- Transient Prompt Configuration End ---
-    '';
-
-    # initContent = lib.mkMerge [
-    #   (lib.mkBefore ''
-    #     # Enable Powerlevel10k instant prompt. Should stay close to the top of ~/.zshrc.
-    #     # Initialization code that may require console input (password prompts, [y/n]
-    #     # confirmations, etc.) must go above this block; everything else may go below.
-    #     if [[ -r "''${XDG_CACHE_HOME:-$HOME/.cache}/p10k-instant-prompt-''${(%):-%n}.zsh" ]]; then
-    #       source "''${XDG_CACHE_HOME:-$HOME/.cache}/p10k-instant-prompt-''${(%):-%n}.zsh"
-    #     fi
-    #   '')
-    #   (lib.mkAfter (lib.readFile ./zshrc))
-    # ];
+    # enable transient prompt
+    initContent =
+      transientPrompt
+      |> lib.optionalString (
+        config.programs.starship.enable && config.programs.starship.enableTransience
+      );
 
     oh-my-zsh = {
       enable = true;
@@ -161,7 +140,7 @@
       ];
       extraConfig = ''
         # Display red dots whilst waiting for completion.
-        COMPLETION_WAITING_DOTS="true"
+                COMPLETION_WAITING_DOTS="true"
       '';
     };
 
--- home/common/optional/starship.nix
+++ home/common/optional/starship.nix
@@ -1,16 +1,50 @@
-{ pkgs, ... }:
+# FIXME: very ascendancy-centric colour customizations atm; modularize with defaults for introdus and allow per user/host optionals
+
+{ config, pkgs, ... }:
+let
+  # HACK: needs assert autostyling and optionalize...  currently have to comment out when binds not used because linting
+  # Bind base16 theme colors to custom vars so we're not restricted to starship's limited named-color tooling,
+  # that inherits from the theme but is limited to only 8 colors
+  # darkestBackground = "#${config.lib.stylix.colors.base00}";     # ----      background
+  darkBackground = "#${config.lib.stylix.colors.base01}"; # ---       lighter background status bar
+  # lightBackground = "#${config.lib.stylix.colors.base02}";       # --        selection background
+  # lightestBackground = "#${config.lib.stylix.colors.base03}";    # -         Comments, Invisibles, Line highlighting
+  # darkestForeground = "#${config.lib.stylix.colors.base04}";     # +         dark foreground status bar
+  # darkForeground = "#${config.lib.stylix.colors.base05}";        # ++        foreground, caret, delimiters, operators
+  # lightForeground = "#${config.lib.stylix.colors.base06}";       # +++       light foreground, rarely used
+  # lightestForeground = "#${config.lib.stylix.colors.base07}";    # ++++      lightest foreground, rarely used
+  # red = "#${config.lib.stylix.colors.base08}";                   # red       vars, xml tags, markup link text, markup lists, diff deleted
+  # orange = "#${config.lib.stylix.colors.base09}";                # orange    Integers, Boolean, Constants, XML Attributes, Markup Link Url
+  # yellow = "#${config.lib.stylix.colors.base0A}";                # yellow    Classes, Markup Bold, Search Text Background
+  # green = "#${config.lib.stylix.colors.base0B}";                 # green     Strings, Inherited Class, Markup Code, Diff Inserted
+  # cyan = "#${config.lib.stylix.colors.base0C}";                  # cyan      Support, Regular Expressions, Escape Characters, Markup Quotes
+  # blue = "#${config.lib.stylix.colors.base0D}";                  # blue      Functions, Methods, Attribute IDs, Headings
+  # purple = "#${config.lib.stylix.colors.base0E}";                # purple    Keywords, Storage, Selector, Markup Italic, Diff Changed
+  darkred = "#${config.lib.stylix.colors.base0F}"; # darkred   Deprecated Highlighting for Methods and Functions, Opening/Closing Embedded Language Tags
+
+  # offThemeGreen = "#5fd700 ";
+  # offThemeRed = "#ff0000 ";
+  # offThemeBlue = "#";
+in
 {
   programs.starship = {
     enable = true;
     package = pkgs.unstable.starship;
-    enableTransience = true;
     enableZshIntegration = true;
+    #Not supported for zsh out of the box, see customization in home/common/core/zsh
+    enableTransience = true;
+
     settings = {
       add_newline = true;
 
+      # some dressing options for ref
+      #░▒▓
+      #▓▒░
+      #
+      #
       format = ''
-        [ ](magenta)$username$hostname$os$directory $git_branch$git_commit$git_state$git_metrics$git_status$nix_shell[ ](magenta)$fill[ ](magenta)$cmd_duration$time[ ](magenta)
-        $character$sudo
+        [░▒▓](${darkBackground})$os$username$hostname$directory$git_branch$git_commit$git_state$git_metrics$git_status$nix_shell[▓▒░](${darkBackground})$fill[░▒▓](${darkBackground})$cmd_duration$time[▓▒░](${darkBackground})
+        $character
       '';
       character = {
         format = "$symbol";
@@ -20,23 +54,26 @@
         disabled = false;
       };
       cmd_duration = {
+        format = "[$duration ]($style)";
+        style = "bg:${darkBackground} fg:white";
         disabled = false;
         min_time = 250;
         show_milliseconds = false;
         show_notifications = false;
-        format = "[$duration](grey) ";
       };
       directory = {
         home_symbol = "~";
-        truncation_length = 8;
+        truncation_length = 9;
         truncation_symbol = "…/";
-        # read_only = " △";
-        read_only_style = "red";
-        style = "blue";
         truncate_to_repo = false;
-        repo_root_format = "[$before_root_path]($before_repo_root_style)[$repo_root]($repo_root_style)[$path]($style)[$read_only]($read_only_style)";
-        before_repo_root_style = "";
-        repo_root_style = "(bold blue)";
+
+        format = "[$path ]($style)[$read_only ]($read_only_style)";
+        style = "bold bg:${darkBackground} fg:blue";
+        read_only_style = "bg:${darkBackground} fg:blue dimmed";
+
+        repo_root_format = "[$before_root_path]($before_repo_root_style)[$repo_root]($repo_root_style)[$path ]($style)[$read_only ]($read_only_style)";
+        before_repo_root_style = "bg:${darkBackground} fg:blue";
+        repo_root_style = "bold bg:${darkBackground} fg:blue";
 
         use_os_path_sep = true;
       };
@@ -47,43 +84,30 @@
         symbol = " ";
       };
       git_branch = {
-        format = "[$symbol$branch(:$remote_branch)]($style) ";
-        symbol = "[ ](green)";
-        style = "green";
+        format = "[$symbol$branch(:$remote_branch) ]($style)";
+        symbol = "[ ](bg:${darkBackground} fg:green)";
+        style = "bg:${darkBackground} fg:green";
       };
       git_status = {
-        #"[△](green)";
-
         format = "($ahead_behind$staged$renamed$modified$untracked$deleted$conflicted$stashed)";
-        conflicted = "[◪$count ](red)";
-        # conflicted = "[~$count](red)";
+        style = "bg:${darkBackground} fg:green";
 
-        ahead = "[▲[$count ](green) ](green)";
-        #ahead = "[⇡[$count ](green) ](green)";
-
-        behind = "[▼[$count ](green) ](green)";
-        # behind = "[⇣[$count ](green) ](green)";
-
-        diverged = "[◇[$ahead_count ](bold green)/[$behind_count](bold red) ](orange)";
-
-        untracked = "[?$count ](yellow)";
-        stashed = "[*$count ](bold purple)";
-
-        #modified = "[●$count ](yellow)";
-        modified = "[!$count ](yellow)";
-
-        # renamed = "[●$count ](blue)";
-        renamed = "[»$count ](cyan)";
-
-        # staged = "[●$count ](bright-cyan)";
-        staged = "[+$count ](blue)";
-        deleted = "[✕$count ](darkred)";
+        conflicted = "[~$count ](bg:${darkBackground} fg:orange)";
+        ahead = "[▲[$count ](bg:${darkBackground} fg:green)](bg:${darkBackground} fg:green)";
+        behind = "[▼[$count ](bg:${darkBackground} fg:green)](bg:${darkBackground} fg:green)";
+        diverged = "[◇[$ahead_count](bold bg:${darkBackground} fg:green)/[$behind_count ](bold bg:${darkBackground} fg:red) ](bg:${darkBackground} fg:orange)";
+        untracked = "[?$count ](bg:${darkBackground} fg:yellow)";
+        stashed = "[*$count ](bold bg:${darkBackground} fg:purple)";
+        modified = "[!$count ](bg:${darkBackground} fg:yellow)";
+        renamed = "[r$count ](bg:${darkBackground} fg:cyan)";
+        staged = "[+$count ](bg:${darkBackground} fg:blue)";
+        deleted = "[-$count ](bg:${darkBackground} fg:red)";
       };
       hostname = {
         disabled = false;
         ssh_only = true;
         format = "[@$hostname]($style)";
-        style = "grey";
+        style = "bg:${darkBackground} fg:${purple}";
       };
       nix_shell = {
         disabled = true;
@@ -95,26 +119,28 @@
       };
       os = {
         disabled = false;
-        format = "[($name)]($style) ";
-        style = "grey";
+        format = "[($name) ]($style)";
+        style = "bg:${darkBackground} fg:${darkred}";
       };
-      sudo = {
-        format = "[$symbol]($style)";
-        style = "red";
-        symbol = "#";
-        disabled = false;
-      };
+      # when enabled this indicates when sudo creds are cached or not
+      # sudo = {
+      #   format = "[$symbol]($style)";
+      #   style = "red";
+      #   symbol = "#";
+      #   disabled = false;
+      # };
       time = {
         disabled = false;
-        format = "$time";
-        time_format = "%y.%m.%d{%H:%M:%S}";
+        format = "[$time]($style)";
+        style = "bg:${darkBackground} fg:${darkred}";
+        time_format = "%y.%m.%d - %H:%M:%S";
       };
       username = {
         disabled = false;
-        style_user = "grey";
-        style_root = "red bold";
-        format = "[$user]($style) ▻ ";
         show_always = false;
+        format = "[$user]($style)";
+        style_user = "bg:${darkBackground} fg:grey";
+        style_root = "bold bg:${darkBackground} fg:red";
       };
     };
   };
