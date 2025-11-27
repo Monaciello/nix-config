29f14298720bdacf1ece8daa0cd20f55fc60d71c
emergentmind <7410928+emergentmind@users.noreply.github.com>
2025-11-26 20:36:24 -0700

    refactor: starting to get DAP set up for nvim

--- home/common/core/nixvim/plugins/dap-ui.nix
+++ home/common/core/nixvim/plugins/dap-ui.nix
@@ -6,10 +6,26 @@
   };
 
   config = lib.mkIf config.nixvim-config.plugins.dap-ui.enable {
-    programs.nixvim.plugins = {
-      dap-ui = {
-        enable = true;
+    programs.nixvim = {
+      plugins = {
+        dap-ui.enable = true;
       };
+      keymaps = [
+        {
+          mode = [ "n" ];
+          key = "<leader>bu";
+          action = config.lib.nixvim.mkRaw ''
+            function()
+            require('dap.ext.vscode').load_launchjs(nil,{})
+            require("dapui").toggle()
+            end
+          '';
+          options = {
+            desc = "DAP- ui";
+            silent = true;
+          };
+        }
+      ];
     };
   };
 }
