2cd3c699a117840ebe1ad4800c59f61d45ed7dea
emergentmind <7410928+emergentmind@users.noreply.github.com>
2025-12-22 09:28:17 -0700

    fix: errant roaming enablement

--- hosts/nixos/gusto/network.nix
+++ hosts/nixos/gusto/network.nix
@@ -1,12 +1,6 @@
-{ config, ... }:
 {
   networking = {
     networkmanager.enable = true;
     enableIPv6 = false;
-  };
-
-  wifi = {
-    enable = true;
-    roaming = config.hostSpec.isRoaming;
   };
 }
